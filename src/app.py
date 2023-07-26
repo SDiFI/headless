921c06d6a173e0743913aa50a851e326a594318a
Smári Guðmundsson <smari@tiro.is>
2023-07-26 16:32:04 +0000

    Removed boilerplate test endpoints.

--- src/app.py
+++ src/app.py
@@ -1,11 +1,9 @@
 import json
 
 from flask import current_app, jsonify
-from flask_apispec import marshal_with, use_kwargs
 from twilio.twiml.voice_response import VoiceResponse
-from webargs.flaskparser import abort
 
-from src import schemas, sock
+from src import sock
 from src.streaming_asr import StreamingASR
 
 
@@ -85,52 +83,3 @@ def route_echo(ws):
     for result in streaming_asr.recognize_stream(feed_data()):
         current_app.logger.info(result)
     current_app.logger.info("CONNECTION CLOSED.")
-
-
-@current_app.route("/foo", methods=["POST"])
-@use_kwargs(schemas.Foo)
-@marshal_with(schemas.Foo, code=201, description="Success return value description.")
-@marshal_with(schemas.Error, code=422, description="Malformed request body")
-@marshal_with(schemas.Error, code=500, description="Internal server error")
-def route_add_foo(**kwargs):
-    try:
-        # Some logic here...
-        return "Posted some foo!", 201
-    except Exception as e:
-        print(f"Failed to post any foo! :(")
-        abort(500)
-
-
-@current_app.route("/hemi", methods=["GET"])
-@marshal_with(
-    schemas.Hemi(many=True),
-    code=200,
-    description="Success return value description.",
-)
-@marshal_with(schemas.Error, code=400, description="Bad request")
-@marshal_with(schemas.Error, code=500, description="Internal server error")
-def route_get_all_hemis():
-    try:
-        return (
-            [
-                {
-                    "demi": 1,
-                    "semi": 2,
-                    "quasi": 3,
-                },
-                {
-                    "demi": 11,
-                    "semi": 12,
-                    "quasi": 13,
-                },
-                {
-                    "demi": 21,
-                    "semi": 22,
-                    "quasi": 23,
-                },
-            ],
-        )
-        200
-    except Exception as e:
-        print(f"Failed to get hemis! :(")
-        abort(500)
